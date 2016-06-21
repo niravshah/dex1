4da4f88b641152bcccaa92ef3e07121b882276c4
niravshah <nirav.shah83@gmail.com>
2016-06-21 20:55:13 +0100

    First

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# Set the base image to Ubuntu
+FROM    node:4-onbuild
+
+# File Author / Maintainer
+MAINTAINER Nirav Shah
+
+# Expose port
+EXPOSE  3000
---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    http = require('http'),
+    mongoose = require('mongoose');
+
+
+var app = express();
+mongoose.connect('mongodb://mongo/myappdatabase');
+
+app.get('/', function(req, res, next) {
+    var evv = process.env.NODE_ENV || 'NONE'
+    res.send('Hello! ' + evv);
+});
+
+http.createServer(app).listen(3000, function() {
+    console.log('Listening on port ' + 3000);
+});
